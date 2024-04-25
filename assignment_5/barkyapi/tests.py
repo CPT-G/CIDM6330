43685e27e6400c61c31f285dbf31bcf026e55e29
CPT-G <jganatta1@buffs.wtamu.edu>
2024-04-25 12:37:47 -0500

    tests update

--- assignment_5/barkyapi/tests.py
+++ assignment_5/barkyapi/tests.py
@@ -103,7 +103,7 @@ class BookmarkTests(APITestCase):
 
 
 class Snippet(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=False)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
